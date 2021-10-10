52f28ade54b763268988dc2ef6a3ae862237e3f7
junsu <melon8372@gmail.com>
2021-10-10 19:38:36 +0900

    feature: Add item to select name, detail, price less-than, price less-than orderBy desc

--- src/main/java/com/meloning/shop/repository/ItemRepository.java
+++ src/main/java/com/meloning/shop/repository/ItemRepository.java
@@ -3,6 +3,11 @@ package com.meloning.shop.repository;
 import com.meloning.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+import java.util.List;
 
+public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findByName(String name);
+    List<Item> findByNameOrDetail(String name, String detail);
+    List<Item> findByPriceLessThan(Integer price);
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
 }
