30df1c19324e1e1039f3a2019183aadda6fa1acc
junsu <melon8372@gmail.com>
2021-10-10 22:56:37 +0900

    feature: Add QueryDsl Predicate

--- src/main/java/com/meloning/shop/repository/ItemRepository.java
+++ src/main/java/com/meloning/shop/repository/ItemRepository.java
@@ -3,11 +3,12 @@ package com.meloning.shop.repository;
 import com.meloning.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
     List<Item> findByName(String name);
     List<Item> findByNameOrDetail(String name, String detail);
     List<Item> findByPriceLessThan(Integer price);
