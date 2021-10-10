45e3f85ec8103f8966dd2416c6469e0b715808af
junsu <melon8372@gmail.com>
2021-10-10 16:58:49 +0900

    first commit

---
+++ src/test/java/com/meloning/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meloning.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
