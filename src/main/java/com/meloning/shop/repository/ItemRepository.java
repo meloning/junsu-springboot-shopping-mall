2e2450d2bfba0c0785e7534476ee0d5f28525dfe
junsu <melon8372@gmail.com>
2021-10-10 19:50:57 +0900

    feature: Add item to select detail JPQL, nativeQuery

--- src/main/java/com/meloning/shop/repository/ItemRepository.java
+++ src/main/java/com/meloning/shop/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.meloning.shop.repository;
 
 import com.meloning.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +12,10 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByNameOrDetail(String name, String detail);
     List<Item> findByPriceLessThan(Integer price);
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    @Query("select i from Item i where i.detail like %:detail% order by i.price desc")
+    List<Item> findByDetail(@Param("detail") String detail);
+
+    @Query(value = "select * from item i where i.detail like %:detail% order by i.price desc", nativeQuery = true)
+    List<Item> findByDetailByNative(@Param("detail") String detail);
 }
