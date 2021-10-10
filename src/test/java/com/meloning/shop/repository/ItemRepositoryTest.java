0331690b5e3b8dc4e470ee60f2d31b6112f15bb2
junsu <melon8372@gmail.com>
2021-10-10 22:56:54 +0900

    test: Add QueryDsl select test-case

--- src/test/java/com/meloning/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/meloning/shop/repository/ItemRepositoryTest.java
@@ -2,24 +2,36 @@ package com.meloning.shop.repository;
 
 import com.meloning.shop.constant.ItemSellStatus;
 import com.meloning.shop.entity.Item;
+import com.meloning.shop.entity.QItem;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.TestPropertySource;
+import org.thymeleaf.util.StringUtils;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.Instant;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.yml")
 class ItemRepositoryTest {
     @Autowired
     ItemRepository itemRepository;
+
+    @PersistenceContext
+    EntityManager entityManager;
 
     @Test
     @DisplayName("상품 저장 테스트")
@@ -42,7 +54,7 @@ class ItemRepositoryTest {
     }
 
     public void createItemList() {
-        for (int i=1; i<=10; i++) {
+        for (int i = 1; i <= 10; i++) {
             Item item = new Item();
             item.setName("테스트 상품" + i);
             item.setPrice(10000 + i);
@@ -151,5 +163,84 @@ class ItemRepositoryTest {
         assertThat(itemList.size()).isEqualTo(10);
         assertThat(itemList).extracting("price")
                 .containsExactly(10010, 10009, 10008, 10007, 10006, 10005, 10004, 10003, 10002, 10001);
+    }
+
+    @Test
+    @DisplayName("Querydsl 조회 테스트1")
+    public void queryDslTest() {
+        // given
+        this.createItemList();
+
+        JPAQueryFactory queryFactory = new JPAQueryFactory(entityManager);
+        QItem qItem = QItem.item;
+        JPAQuery<Item> query = queryFactory.selectFrom(qItem)
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qItem.detail.like("%" + "테스트 상품 상세 설명" + "%"))
+                .orderBy(qItem.price.desc());
+
+        // when
+        List<Item> itemList = query.fetch();
+
+        // then
+        assertThat(itemList.size()).isEqualTo(10);
+        assertThat(itemList).extracting("price")
+                .containsExactly(10010, 10009, 10008, 10007, 10006, 10005, 10004, 10003, 10002, 10001);
+    }
+
+    public void createItemList2() {
+        for (int i = 1; i <= 5; i++) {
+            Item item = new Item();
+            item.setName("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStock(100);
+            item.setCreatedDate(Instant.now());
+            item.setUpdatedDate(Instant.now());
+            Item savedItem = itemRepository.save(item);
+        }
+        for (int i = 6; i <= 10; i++) {
+            Item item = new Item();
+            item.setName("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SOLD_OUT);
+            item.setStock(100);
+            item.setCreatedDate(Instant.now());
+            item.setUpdatedDate(Instant.now());
+            Item savedItem = itemRepository.save(item);
+        }
+    }
+
+    @Test
+    @DisplayName("상품 Querydsl 조회 테스트 2")
+    public void queryDslTest2() {
+        // given
+        this.createItemList2();
+
+        final String testDetail = "테스트 상품 상세 설명";
+        final int testPrice = 10003;
+        final String testItemSellState = "SELL";
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QItem item = QItem.item;
+        booleanBuilder.and(item.detail.like("%" + testDetail + "%"));
+        booleanBuilder.and(item.price.gt(testPrice));
+
+        if(StringUtils.equals(testItemSellState, ItemSellStatus.SELL)) {
+            booleanBuilder.and(item.itemSellStatus.eq(ItemSellStatus.SELL));
+        }
+
+        Pageable pageable = PageRequest.of(0, 5);
+        Page<Item> itemPagingResult = itemRepository.findAll(booleanBuilder, pageable);
+        System.out.println(String.format("Total Element : %s", itemPagingResult.getTotalElements()));
+
+        List<Item> resultItemList = itemPagingResult.getContent();
+        assertThat(resultItemList.size()).isEqualTo(2);
+        assertThat(resultItemList).extracting("price", "itemSellStatus")
+                .containsExactly(
+                        tuple(10004, ItemSellStatus.SELL),
+                        tuple(10005, ItemSellStatus.SELL)
+                );
     }
 }
