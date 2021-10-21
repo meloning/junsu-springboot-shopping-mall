9ca5e61a98d71c4f16c3e09ac78ace621fc0d06a
junsu <melon8372@gmail.com>
2021-10-21 19:13:11 +0900

    feature: Add item detail page process

--- src/main/java/com/meloning/shop/controller/ItemController.java
+++ src/main/java/com/meloning/shop/controller/ItemController.java
@@ -1,105 +1,22 @@
 package com.meloning.shop.controller;
 
 import com.meloning.shop.dto.ItemFormDto;
-import com.meloning.shop.dto.ItemSearchDto;
-import com.meloning.shop.entity.Item;
 import com.meloning.shop.service.ItemService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.persistence.EntityNotFoundException;
-import javax.validation.Valid;
-import java.util.List;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
 public class ItemController {
     private final ItemService itemService;
 
-    @GetMapping(value = {"/admin/items", "/admin/items/{page}"})
-    public String itemManage(ItemSearchDto itemSearchDto,
-                             @PathVariable("page") Optional<Integer> page,
-                             Model model) {
-        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 3);
-        Page<Item> items = itemService.getAdminItemPage(itemSearchDto, pageable);
-        model.addAttribute("items", items);
-        model.addAttribute("itemSearchDto", itemSearchDto);
-        model.addAttribute("maxPage", 5);
-        return "item/itemManage";
-    }
-
-    @GetMapping("/admin/item/new")
-    public String itemForm(Model model) {
-        model.addAttribute("itemFormDto", new ItemFormDto());
-        return "/item/itemForm";
-    }
-
-    @PostMapping("/admin/item/new")
-    public String itemNew(@Valid ItemFormDto itemFormDto,
-                          BindingResult bindingResult,
-                          Model model,
-                          @RequestParam("itemImageFile") List<MultipartFile> itemImageFileList) {
-        if (bindingResult.hasErrors()) {
-            return "item/itemForm";
-        }
-
-        if (itemImageFileList.get(0).isEmpty() && itemFormDto.getId() == null) {
-            model.addAttribute("errorMessage", "첫번째 상품 이미지는 필수 입력 값입니다.");
-            return "item/itemForm";
-        }
-
-        try {
-            itemService.saveItem(itemFormDto, itemImageFileList);
-        } catch (Exception e) {
-            model.addAttribute("errorMessage", "상품 등록 중 에러가 발생하였습니다.");
-            return "item/itemForm";
-        }
-
-        return "redirect:/";
-    }
-
-    @GetMapping("/admin/item/{itemId}")
+    @GetMapping("/item/{itemId}")
     public String itemDetail(@PathVariable("itemId") Long itemId, Model model) {
-        try {
-            ItemFormDto itemFormDto = itemService.getItemDetail(itemId);
-            model.addAttribute("itemFormDto", itemFormDto);
-        } catch (EntityNotFoundException e) {
-            model.addAttribute("errorMessage", "존재하지 않는 상품입니다.");
-            model.addAttribute("itemFormDto", new ItemFormDto());
-            return "item/itemForm";
-        }
-        return "item/itemForm";
-    }
-
-    @PostMapping("/admin/item")
-    public String itemUpdate(@Valid ItemFormDto itemFormDto,
-                             BindingResult bindingResult,
-                             @RequestParam("itemImageFile") List<MultipartFile> itemImageFileList,
-                             Model model) {
-        if (bindingResult.hasErrors()) {
-            return "item/itemForm";
-        }
-
-        if (itemImageFileList.get(0).isEmpty() && itemFormDto.getId() == null) {
-            model.addAttribute("errorMessage", "첫번째 상품 이미지는 필수 입력 값입니다.");
-            return "item/itemForm";
-        }
-
-        try {
-            itemService.updateItem(itemFormDto, itemImageFileList);
-        } catch (Exception e) {
-            model.addAttribute("errorMessage", "상품 수정 중 에러가 발생하였습니다.");
-            return "item/itemForm";
-        }
-
-        return "redirect:/";
+        ItemFormDto itemFormDto = itemService.getItemDetail(itemId);
+        model.addAttribute("item", itemFormDto);
+        return "item/itemDetail";
     }
 }
