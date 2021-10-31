251ad8247cf0e92f0f209b7af0fefa5298707c73
junsu <melon8372@gmail.com>
2021-10-31 18:05:42 +0900

    test: Add order process test-case

---
+++ src/test/java/com/meloning/shop/service/OrderServiceTest.java
@@ -0,0 +1,79 @@
+package com.meloning.shop.service;
+
+import com.meloning.shop.constant.ItemSellStatus;
+import com.meloning.shop.dto.OrderDto;
+import com.meloning.shop.entity.Item;
+import com.meloning.shop.entity.Member;
+import com.meloning.shop.entity.Order;
+import com.meloning.shop.entity.OrderItem;
+import com.meloning.shop.repository.ItemRepository;
+import com.meloning.shop.repository.MemberRepository;
+import com.meloning.shop.repository.OrderRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.yml")
+class OrderServiceTest {
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+
+    public Item saveItem(){
+        Item item = new Item();
+        item.setName("테스트 상품");
+        item.setPrice(10000);
+        item.setDetail("테스트 상품 상세 설명");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        item.setStock(100);
+        return itemRepository.save(item);
+    }
+
+    public Member saveMember(){
+        Member member = new Member();
+        member.setEmail("test@test.com");
+        return memberRepository.save(member);
+
+    }
+
+    @Test
+    @DisplayName("주문 테스트")
+    public void order(){
+        Item item = saveItem();
+        Member member = saveMember();
+
+        OrderDto orderDto = new OrderDto();
+        orderDto.setCount(10);
+        orderDto.setItemId(item.getId());
+
+        Long orderId = orderService.order(orderDto, member.getEmail());
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(EntityNotFoundException::new);
+
+        List<OrderItem> orderItems = order.getOrderItems();
+
+        int totalPrice = orderDto.getCount()*item.getPrice();
+
+        assertThat(totalPrice).isEqualTo(order.getTotalPrice());
+    }
+}
